d96f6b333ed39c750eeae3f54b103b11f2e76d88
Axel Galvan <71230877+AVGalvan@users.noreply.github.com>
2023-06-02 09:23:05 -0700

    create playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,5 +5,30 @@ const getComputerChoice = () => {
   return(choices[randomInt])
 }
 
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = toLowerCase(playerSelection);
 
+  if(playerSelection === computerSelection){
+    return 'Tie! You both played the same option'
+  } else if (playerSelection === 'rock'){
+    if (computerSelection === 'scissors'){
+      return 'You win! Computer chose scissors.';
+    } else {
+      return 'You lose! Computer chose paper.';
+    }
+  } else if (playerSelection === 'paper'){
+    if(computerSelection === 'rock'){
+      return 'You win! Computer chose rock.'
+    } else {
+      return 'You lose! Computer chose scissors.'
+    }
+  } else if (playerSelection === 'scissors'){
+    if(computerSelection === 'paper'){
+      return 'You win! Computer chose paper.'
+    } else {
+      return 'You lose! Computer chose rock.'
+    }
+  }
+
+}
 
